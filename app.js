5ee2b92f0f0b61a4f66fe0846a5b9d18aee22f28
abidmmp <abidmmp99@gmail.com>
2018-03-08 09:00:00 +0500

    Optimize range performance

--- app.js
+++ app.js
@@ -116,3 +116,10 @@ const copyToClipboard = async (text) => {
 
 const queryParams = (params) =>
   '?' + new URLSearchParams(params).toString();
+
+
+const range = (start, end, step = 1) => {
+  const result = [];
+  for (let i = start; i < end; i += step) result.push(i);
+  return result;
+};
