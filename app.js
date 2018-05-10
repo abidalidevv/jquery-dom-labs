bc25bf873c377b3ec76aa28773bdf836bff87e49
abidmmp <abidmmp99@gmail.com>
2018-05-10 10:17:00 +0500

    Update compose helper method

--- app.js
+++ app.js
@@ -147,3 +147,6 @@ const storage = {
   set: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
   remove: (key) => localStorage.removeItem(key),
 };
+
+
+const compose = (...fns) => (value) => fns.reduceRight((v, fn) => fn(v), value);
