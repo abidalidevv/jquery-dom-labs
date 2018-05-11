3196a9ba27a76bfd36598a3caba6a0863060059d
abidmmp <abidmmp99@gmail.com>
2018-05-11 11:34:00 +0500

    Refactor pick logic

--- app.js
+++ app.js
@@ -154,3 +154,7 @@ const compose = (...fns) => (value) => fns.reduceRight((v, fn) => fn(v), value);
 
 const capitalize = (str) =>
   str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+
+
+const pick = (obj, keys) =>
+  Object.fromEntries(keys.filter((k) => k in obj).map((k) => [k, obj[k]]));
