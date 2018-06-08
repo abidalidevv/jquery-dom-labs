7d2a1729f68100ce988d91b0d398bb8876fb8f01
abidmmp <abidmmp99@gmail.com>
2018-06-08 11:34:00 +0500

    Implement EventEmitter functionality

--- helpers.js
+++ helpers.js
@@ -192,3 +192,20 @@ const compose = (...fns) => (value) => fns.reduceRight((v, fn) => fn(v), value);
 
 
 const pipe = (...fns) => (value) => fns.reduce((v, fn) => fn(v), value);
+
+
+class EventEmitter {
+  constructor() { this._events = {}; }
+  on(event, listener) {
+    (this._events[event] = this._events[event] || []).push(listener);
+    return this;
+  }
+  off(event, listener) {
+    this._events[event] = (this._events[event] || []).filter(l => l !== listener);
+    return this;
+  }
+  emit(event, ...args) {
+    (this._events[event] || []).forEach(l => l(...args));
+    return this;
+  }
+}
