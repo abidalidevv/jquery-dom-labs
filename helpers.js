87f2df1b21d4b024bf26ef3dd56d7248fbe8a874
abidmmp <abidmmp99@gmail.com>
2018-06-08 09:00:00 +0500

    Refactor pipe logic

--- helpers.js
+++ helpers.js
@@ -189,3 +189,6 @@ const getNestedValue = (obj, path, fallback = undefined) =>
 
 
 const compose = (...fns) => (value) => fns.reduceRight((v, fn) => fn(v), value);
+
+
+const pipe = (...fns) => (value) => fns.reduce((v, fn) => fn(v), value);
