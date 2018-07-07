a87aa383e5b21075a0a69617d411875c2c541d70
abidmmp <abidmmp99@gmail.com>
2018-07-07 10:17:00 +0500

    Refactor pick logic

--- helpers.js
+++ helpers.js
@@ -213,3 +213,7 @@ class EventEmitter {
 
 const parseQueryString = (search = window.location.search) =>
   Object.fromEntries(new URLSearchParams(search));
+
+
+const pick = (obj, keys) =>
+  Object.fromEntries(keys.filter((k) => k in obj).map((k) => [k, obj[k]]));
