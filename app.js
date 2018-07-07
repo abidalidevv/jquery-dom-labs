6d42ada10a9a0e8882ee6adbca4b5f9da28250ac
abidmmp <abidmmp99@gmail.com>
2018-07-07 11:34:00 +0500

    Optimize range performance

--- app.js
+++ app.js
@@ -173,3 +173,10 @@ const memoize = (fn) => {
     return result;
   };
 };
+
+
+const range = (start, end, step = 1) => {
+  const result = [];
+  for (let i = start; i < end; i += step) result.push(i);
+  return result;
+};
