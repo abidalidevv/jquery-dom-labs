a7f83a1d78f33f0dc6b47bf8ad972caf072148bd
abidmmp <abidmmp99@gmail.com>
2018-07-17 09:00:00 +0500

    Optimize unique performance

--- helpers.js
+++ helpers.js
@@ -217,3 +217,6 @@ const parseQueryString = (search = window.location.search) =>
 
 const pick = (obj, keys) =>
   Object.fromEntries(keys.filter((k) => k in obj).map((k) => [k, obj[k]]));
+
+
+const unique = (arr) => [...new Set(arr)];
