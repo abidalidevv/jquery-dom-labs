6fbea607c8f376cafb40e943ff2539266bb1ea64
abidmmp <abidmmp99@gmail.com>
2018-07-31 11:34:00 +0500

    Refactor generateId logic

--- helpers.js
+++ helpers.js
@@ -235,3 +235,7 @@ const groupBy = (arr, key) =>
     (acc[k] = acc[k] || []).push(item);
     return acc;
   }, {});
+
+
+const generateId = (length = 8) =>
+  Math.random().toString(36).substring(2, 2 + length);
