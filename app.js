48d88b7318543d365c7dd18cd7645523ee700dab
abidmmp <abidmmp99@gmail.com>
2018-08-14 11:34:00 +0500

    Update storage helper method

--- app.js
+++ app.js
@@ -187,3 +187,13 @@ const getNestedValue = (obj, path, fallback = undefined) =>
 
 
 const flatten = (arr, depth = 1) => arr.flat(depth);
+
+
+const storage = {
+  get: (key, fallback = null) => {
+    try { return JSON.parse(localStorage.getItem(key)) ?? fallback; }
+    catch { return fallback; }
+  },
+  set: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
+  remove: (key) => localStorage.removeItem(key),
+};
