05769092b24febc16e90210b6f0fbaaf726ccb68
abidmmp <abidmmp99@gmail.com>
2018-11-02 09:00:00 +0500

    Implement setNestedValue functionality

--- helpers.js
+++ helpers.js
@@ -355,3 +355,12 @@ const parseQueryString = (search = window.location.search) =>
 
 const generateId = (length = 8) =>
   Math.random().toString(36).substring(2, 2 + length);
+
+
+const setNestedValue = (obj, path, value) => {
+  const keys = path.split('.');
+  const last = keys.pop();
+  const target = keys.reduce((acc, key) => (acc[key] = acc[key] || {}), obj);
+  target[last] = value;
+  return obj;
+};
