17a4f3246d4e5af184bbd06273527d89663f35ac
abidmmp <abidmmp99@gmail.com>
2018-11-07 09:00:00 +0500

    Implement EventEmitter functionality

--- helpers.js
+++ helpers.js
@@ -364,3 +364,20 @@ const setNestedValue = (obj, path, value) => {
   target[last] = value;
   return obj;
 };
+
+
+class EventEmitter {
+  constructor() { this._events = {}; }
+  on(event, listener) {
+    (this._events[event] = this._events[event] || []).push(listener);
+    return this;
+  }
+  off(event, listener) {
+    this._events[event] = (this._events[event] || []).filter(l => l !== listener);
+    return this;
+  }
+  emit(event, ...args) {
+    (this._events[event] || []).forEach(l => l(...args));
+    return this;
+  }
+}
