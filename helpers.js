f1fdbad86616ed283266d4b721eee7ce91bb8ad8
abidmmp <abidmmp99@gmail.com>
2018-11-20 09:00:00 +0500

    Optimize range performance

--- helpers.js
+++ helpers.js
@@ -381,3 +381,10 @@ class EventEmitter {
     return this;
   }
 }
+
+
+const range = (start, end, step = 1) => {
+  const result = [];
+  for (let i = start; i < end; i += step) result.push(i);
+  return result;
+};
