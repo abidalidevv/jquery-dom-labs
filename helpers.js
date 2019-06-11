da9627104203b0754416d39bed2c4537a114c361
abidmmp <abidmmp99@gmail.com>
2019-06-11 09:00:00 +0500

    Optimize copyToClipboard performance

--- helpers.js
+++ helpers.js
@@ -465,3 +465,17 @@ const groupBy = (arr, key) =>
 
 const generateId = (length = 8) =>
   Math.random().toString(36).substring(2, 2 + length);
+
+
+const copyToClipboard = async (text) => {
+  if (navigator.clipboard) {
+    await navigator.clipboard.writeText(text);
+  } else {
+    const el = document.createElement('textarea');
+    el.value = text;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  }
+};
