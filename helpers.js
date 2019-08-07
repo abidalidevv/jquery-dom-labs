06d712345383d5db884e4a5216d93b56238347ef
abidmmp <abidmmp99@gmail.com>
2019-08-07 09:00:00 +0500

    Update groupBy helper method

--- helpers.js
+++ helpers.js
@@ -487,3 +487,11 @@ const omit = (obj, keys) =>
 
 const toKebabCase = (str) =>
   str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+
+
+const groupBy = (arr, key) =>
+  arr.reduce((acc, item) => {
+    const k = typeof key === 'function' ? key(item) : item[key];
+    (acc[k] = acc[k] || []).push(item);
+    return acc;
+  }, {});
