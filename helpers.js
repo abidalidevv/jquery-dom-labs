0e2277393e658fcf186c05e2ede0ace42e37c129
abidmmp <abidmmp99@gmail.com>
2019-08-17 09:00:00 +0500

    Refactor pick logic

--- helpers.js
+++ helpers.js
@@ -499,3 +499,7 @@ const groupBy = (arr, key) =>
 
 const pick = (obj, keys) =>
   Object.fromEntries(keys.filter((k) => k in obj).map((k) => [k, obj[k]]));
+
+
+const pick = (obj, keys) =>
+  Object.fromEntries(keys.filter((k) => k in obj).map((k) => [k, obj[k]]));
