461de3fbc2a6a1fcf38d995517c5c84ae36fb9c8
abidmmp <abidmmp99@gmail.com>
2020-03-04 10:17:00 +0500

    Refactor retry logic

--- helpers.js
+++ helpers.js
@@ -527,3 +527,15 @@ const groupBy = (arr, key) =>
     (acc[k] = acc[k] || []).push(item);
     return acc;
   }, {});
+
+
+const retry = async (fn, attempts = 3, delay = 500) => {
+  for (let i = 0; i < attempts; i++) {
+    try {
+      return await fn();
+    } catch (err) {
+      if (i === attempts - 1) throw err;
+      await sleep(delay * (i + 1));
+    }
+  }
+};
