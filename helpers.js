ba9ea0099c3e8a060b54fe2f63420e71c90382b9
abidmmp <abidmmp99@gmail.com>
2020-08-05 09:00:00 +0500

    Update storage helper method

--- helpers.js
+++ helpers.js
@@ -600,3 +600,13 @@ const throttle = (fn, limit) => {
 
 
 const deepClone = (obj) => JSON.parse(JSON.stringify(obj));
+
+
+const storage = {
+  get: (key, fallback = null) => {
+    try { return JSON.parse(localStorage.getItem(key)) ?? fallback; }
+    catch { return fallback; }
+  },
+  set: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
+  remove: (key) => localStorage.removeItem(key),
+};
