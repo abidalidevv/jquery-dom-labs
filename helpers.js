db188bc384882a2cbf41553c07975435c6a7318a
abidmmp <abidmmp99@gmail.com>
2020-08-31 09:00:00 +0500

    Update chunk helper method

--- helpers.js
+++ helpers.js
@@ -614,3 +614,9 @@ const storage = {
 
 const toKebabCase = (str) =>
   str.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+
+
+const chunk = (arr, size) =>
+  Array.from({ length: Math.ceil(arr.length / size) }, (_, i) =>
+    arr.slice(i * size, i * size + size)
+  );
