50c4f0cbe8e2b736b0afb9c8528e816fda57b9da
abidmmp <abidmmp99@gmail.com>
2021-05-24 10:17:00 +0500

    Refactor retry logic

--- helpers.js
+++ helpers.js
@@ -647,3 +647,15 @@ const scrollToTop = (smooth = true) =>
 
 const scrollToTop = (smooth = true) =>
   window.scrollTo({ top: 0, behavior: smooth ? 'smooth' : 'auto' });
+
+
+const retry = async (fn, attempts = 3, delay = 500) => {
+  for (let i = 0; i < attempts; i++) {
+    try {
+      return await fn();
+    } catch (err) {
+      if (i === attempts - 1) throw err;
+      await sleep(delay * (i + 1));
+    }
+  }
+};
