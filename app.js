c87ddd484d081997a62a9a06f1b8616bdf1e5876
abidmmp <abidmmp99@gmail.com>
2021-07-08 10:17:00 +0500

    Implement EventEmitter functionality

--- app.js
+++ app.js
@@ -476,3 +476,20 @@ const formatDate = (date, locale = 'en-US', options = {}) =>
     year: 'numeric', month: 'short', day: 'numeric',
     ...options,
   }).format(new Date(date));
+
+
+class EventEmitter {
+  constructor() { this._events = {}; }
+  on(event, listener) {
+    (this._events[event] = this._events[event] || []).push(listener);
+    return this;
+  }
+  off(event, listener) {
+    this._events[event] = (this._events[event] || []).filter(l => l !== listener);
+    return this;
+  }
+  emit(event, ...args) {
+    (this._events[event] || []).forEach(l => l(...args));
+    return this;
+  }
+}
