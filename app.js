b091cf6fca41d3172a83b34e9334a0ebbd3dda40
abidmmp <abidmmp99@gmail.com>
2021-08-18 10:17:00 +0500

    Implement EventEmitter functionality

--- app.js
+++ app.js
@@ -493,3 +493,20 @@ class EventEmitter {
     return this;
   }
 }
+
+
+class EventEmitter {
+  constructor() { this._events = {}; }
+  on(event, listener) {
+    (this._events[event] = this._events[event] || []).push(listener);
+    return this;
+  }
+  off(event, listener) {
+    this._events[event] = (this._events[event] || []).filter(l => l !== listener);
+    return this;
+  }
+  emit(event, ...args) {
+    (this._events[event] || []).forEach(l => l(...args));
+    return this;
+  }
+}
