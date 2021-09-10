5e0a625d4ca5d5a55de65083fffda57ba9edef99
abidmmp <abidmmp99@gmail.com>
2021-09-10 10:17:00 +0500

    Update storage helper method

--- app.js
+++ app.js
@@ -518,3 +518,13 @@ const generateId = (length = 8) =>
 
 const parseQueryString = (search = window.location.search) =>
   Object.fromEntries(new URLSearchParams(search));
+
+
+const storage = {
+  get: (key, fallback = null) => {
+    try { return JSON.parse(localStorage.getItem(key)) ?? fallback; }
+    catch { return fallback; }
+  },
+  set: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
+  remove: (key) => localStorage.removeItem(key),
+};
