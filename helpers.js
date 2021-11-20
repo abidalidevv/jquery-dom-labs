6e897d797605f5e9201516ae8d72bbf1df2ac7e1
abidmmp <abidmmp99@gmail.com>
2021-11-20 09:00:00 +0500

    Optimize copyToClipboard performance

--- helpers.js
+++ helpers.js
@@ -706,3 +706,17 @@ const formatCurrency = (amount, currency = 'USD', locale = 'en-US') =>
 
 const getNestedValue = (obj, path, fallback = undefined) =>
   path.split('.').reduce((acc, key) => acc?.[key], obj) ?? fallback;
+
+
+const copyToClipboard = async (text) => {
+  if (navigator.clipboard) {
+    await navigator.clipboard.writeText(text);
+  } else {
+    const el = document.createElement('textarea');
+    el.value = text;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  }
+};
