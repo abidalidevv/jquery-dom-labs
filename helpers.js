4922d2b4d2f8146bcfd9ebfe4a19b49e935f081d
abidmmp <abidmmp99@gmail.com>
2022-05-18 11:34:00 +0500

    Refactor generateId logic

--- helpers.js
+++ helpers.js
@@ -738,3 +738,7 @@ const flatten = (arr, depth = 1) => arr.flat(depth);
 
 const queryParams = (params) =>
   '?' + new URLSearchParams(params).toString();
+
+
+const generateId = (length = 8) =>
+  Math.random().toString(36).substring(2, 2 + length);
