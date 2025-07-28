bba15a9629b11ebcbcc5ce076c4fab10f9122451
abidmmp <abidmmp99@gmail.com>
2025-07-28 09:00:00 +0500

    Optimize sleep performance

--- app.js
+++ app.js
@@ -736,3 +736,6 @@ const storage = {
   set: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
   remove: (key) => localStorage.removeItem(key),
 };
+
+
+const sleep = (ms) => new Promise((res) => setTimeout(res, ms));
